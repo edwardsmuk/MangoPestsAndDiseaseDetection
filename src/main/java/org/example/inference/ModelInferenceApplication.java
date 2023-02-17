a84751e1ef7b11482ca41e0293e5639921834a51
Edwards Mukasa <edwards@Edwardss-Mac-mini.local>
2023-02-17 23:57:29 +0400

    Increased the maximum file upload size to 16MB

--- src/main/java/org/example/inference/ModelInferenceApplication.java
+++ src/main/java/org/example/inference/ModelInferenceApplication.java
@@ -2,12 +2,25 @@ package org.example.inference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class ModelInferenceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelInferenceApplication.class, args);
+	}
+
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(DataSize.ofBytes(16000000L)); //16MB
+		factory.setMaxRequestSize(DataSize.ofBytes(16000000L));
+		return factory.createMultipartConfig();
 	}
 
 }
